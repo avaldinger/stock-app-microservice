9218b41ede0a6f9a8af6b1425cc34b7cd22d5bc7
avaldinger <adam.valdinger@gmail.com>
2021-05-16 15:37:16 +0200

    Accounts and AccountBalances classes added from the monolith

---
+++ customer-service/src/main/java/Table/Account.java
@@ -0,0 +1,134 @@
+package Table;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	@Column(name = "owner_type")
+	private String ownerType;
+
+
+	@OneToMany(mappedBy = "accounts", cascade = { CascadeType.ALL }, fetch = FetchType.LAZY)
+	private List<AccountBalances> accountBalances;
+
+
+	public Account() {
+	}
+
+	public Account(String firstName, String lastName, String ownerType) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.ownerType = ownerType;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getOwnerType() {
+		return ownerType;
+	}
+
+	public void setOwnerType(String ownerType) {
+		this.ownerType = ownerType;
+	}
+
+	@Override
+	public String toString() {
+		return "Accounts id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", ownerType=" + ownerType;
+	}
+
+	public List<Portfolios> getPortfolios() {
+		return portfolios;
+	}
+
+	public void setPortfolios(List<Portfolios> portfolios) {
+		this.portfolios = portfolios;
+	}
+
+	public List<AccountBalances> getAccountBalances() {
+		return accountBalances;
+	}
+
+	public void setAccountBalances(List<AccountBalances> accountBalances) {
+		this.accountBalances = accountBalances;
+	}
+
+	public List<Orders> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Orders> orders) {
+		this.orders = orders;
+	}
+
+	// convenience method to add portfolios
+	public void addPortfolio(Portfolios tempPortfolio) {
+
+		if (portfolios == null) {
+			portfolios = new ArrayList<>();
+		}
+
+		portfolios.add(tempPortfolio);
+
+		tempPortfolio.setAccounts(this);
+	}
+
+	// convenience method to add portfolios
+	public void addOrders(Orders tempOrders) {
+
+		if (orders == null) {
+			orders = new ArrayList<>();
+		}
+
+		orders.add(tempOrders);
+
+		tempOrders.setAccounts(this);
+	}
+
+	// convenience method to add portfolios
+	public void addAccountBalance(AccountBalances tempAccountBalance) {
+
+		if (accountBalances == null) {
+			accountBalances = new ArrayList<>();
+		}
+
+		accountBalances.add(tempAccountBalance);
+
+		tempAccountBalance.setAccounts(this);
+	}
+
+}
---
+++ customer-service/src/main/java/Table/AccountBalances.java
@@ -0,0 +1,89 @@
+package Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="account_balances")
+public class AccountBalances {
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	@Column(name="amount")
+	private int amount;
+	@Column(name="currency")
+	private String currency;
+	@Column(name="balance_available")
+	private boolean balanceAvailable;
+
+	
+	@ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+	@JoinColumn(name = "fk_account_id_account_balances")
+	// Annotation to avoid JSON loop when calling 
+	// all the info about the Account
+	@JsonBackReference
+	private Account accounts;
+	
+	
+	public AccountBalances() {}
+
+	public AccountBalances(int amount, String currency, boolean balanceAvailable, Account account) {
+		this.amount = amount;
+		this.currency = currency;
+		this.balanceAvailable = balanceAvailable;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	
+	public boolean isBalanceAvailable() {
+		return balanceAvailable;
+	}
+
+	public void setBalanceAvailable(boolean balanceAvailable) {
+		this.balanceAvailable = balanceAvailable;
+	}
+
+	@Override
+	public String toString() {
+		return "AccountBalances: id=" + id + ", amount=" + amount + ", currency=" + currency + ", balanceAvailable="
+				+ balanceAvailable;
+	}
+
+	public Account getAccounts() {
+		return accounts;
+	}
+
+	public void setAccounts(Account accounts) {
+		this.accounts = accounts;
+	}
+	
+
+
+}
